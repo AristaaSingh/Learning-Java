12b1f0d1025f109d6dd6f686929405fad1176cbc
Aristaa Singh <146640656+AristaaSingh@users.noreply.github.com>
2024-12-13 03:33:41 +0000

    Hello World!

---
+++ section1/HelloJava.java
@@ -0,0 +1,10 @@
+// all code in java goes inside a class
+// class name should be the same as filename
+
+public class HelloJava {
+    // main: entry point of program
+    public static void main(String[] args) {
+        // semi colon is end of statement
+        System.out.println("Hello World");
+    }
+}
