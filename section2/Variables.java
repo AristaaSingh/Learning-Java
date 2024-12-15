5cbd9fa2884d7db88b6569f84234c0ecf0ae6678
Aristaa Singh <146640656+AristaaSingh@users.noreply.github.com>
2024-12-15 07:25:07 +0000

    variables

---
+++ section2/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // int stype stores 4 bytes
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+    }
+}
