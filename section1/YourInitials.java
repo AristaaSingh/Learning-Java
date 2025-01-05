a33fd4bafd20ddc7eed824130b3ac824984bcf69
Aristaa Singh <146640656+AristaaSingh@users.noreply.github.com>
2025-01-05 17:05:45 +0000

    what is happening

---
+++ section1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("  A     SSS ");
+         System.out.println(" A A   S   S");
+         System.out.println("A   A  S    ");
+         System.out.println("AAAAA   SSS ");
+         System.out.println("A   A      S");
+         System.out.println("A   A  S   S");
+         System.out.println("A   A   SSS ");
+    }
+}
